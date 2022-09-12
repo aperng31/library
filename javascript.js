674b1251a0d8727006cba31141468365a36673fc
Alan Perng <aperng31@gmail.com>
2022-09-12 13:11:56 -0700

    Switch from using class/constructors to factory functions/IIFE

--- javascript.js
+++ javascript.js
@@ -1,122 +1,152 @@
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages; 
-        this.read = read;
-        //this.id added in loadTable
-    }
-    changeRead() {
-        if (this.read == false) {
-            this.read = true;
+const Book = (myTitle, myAuthor, myPages, myRead) => {
+    let title = myTitle;
+    let author = myAuthor;
+    let pages = myPages; 
+    let read = myRead;
+    let id;
+    //id added in loadTable
+    const changeRead = () => {
+        if (read == false) {
+            read = true;
         }
         else {
-            this.read = false;
+            read = false;
         }
         console.log('changed!');
     }
-    deleteBook() {
-        // const delBook = book.path[2]; //is a pointer, inspect browser <<DONT NEED
-        const tempInd = this.id;    //id of book corresponds to index in myLibrary
-        const nChild = +tempInd + 1;    //child counts starting at 1, not 0
+    const getTitle = () => {
+        return title;
+    }
+    const getAuthor = () => {
+        return author;
+    }
+    const getPages = () => {
+        return pages;
+    }
+    const getRead = () => {
+        return read;
+    }
+    const setID = (myId) => {
+        id = myId;
+    }
+    const getID = () => {
+        return id;
+    }
+    return { changeRead, getTitle, getAuthor, getPages, getRead, setID, getID };
+}
 
-        const delBook = document.querySelector(`#tbody tr:nth-child(${nChild})`);
-        
-        delBook.remove(); //remove corres tr from table
-        myLibrary.splice(tempInd, 1); //remove at index
-        myDelButtons.splice(tempInd, 1);
+const libraryCreator = (() => {
+    let myLibrary = [];
 
-        console.log(myLibrary);
-        
-        for (let i = tempInd; i < myLibrary.length; i++) {//need to update id of succeeding objects -1
-            myDelButtons[i].id = i; //correspond button id to index and row
-            myLibrary[i].id = i;    //correspond book id to index and row
+    const loadBook = (book) => {
+        myLibrary.push(book);
+        book.setID(myLibrary.indexOf(book) + 1);
+        //console.log(book.getID());
+        return book.getID();
+    }
+    const deleteBook = (id) => {
+        myLibrary.splice(id - 1, 1);
+        shiftID(id - 1);
+    }
+    const getLib = () => {
+        return myLibrary;
+    }
+    const shiftID = (pos) => {
+        for (let i = pos; i < myLibrary.length; i++) {//need to update id of succeeding objects -1
+            myLibrary[i].setID(i);    //reset book id to new index and row
         }
-        console.log(myDelButtons);
     }
-}
 
-function loadTable(book) {
-    //add information to table, make sure to include class of certain 
-    const newTr = document.createElement('tr');
+    return { loadBook, deleteBook, getLib };
+})();
 
-    const titleTd = document.createElement('td');
-    titleTd.innerHTML = book.title; 
-    newTr.appendChild(titleTd);
+const DOM_manager = (() => {
 
-    const authorTd = document.createElement('td');
-    authorTd.innerHTML = book.author;
-    newTr.appendChild(authorTd)
+    let tbody = document.querySelector('#tbody');
 
-    const pagesTd = document.createElement('td');
-    pagesTd.innerHTML = book.pages;
-    newTr.appendChild(pagesTd);
+    const loadTable = (book) => { //creates new tr and td to load book info into tbody
+        const newTr = document.createElement('tr');
 
-    const readTd = document.createElement('td');
-    if (book.read == true) {
-        readTd.innerHTML = "<input type=\"checkbox\" class=\"read\" checked></input>"
-    } 
-    else {
-        readTd.innerHTML = "<input type=\"checkbox\" class=\"read\"></input>"
+        const titleTd = document.createElement('td');
+        titleTd.innerHTML = book.getTitle(); 
+        newTr.appendChild(titleTd);
+
+        const authorTd = document.createElement('td');
+        authorTd.innerHTML = book.getAuthor();
+        newTr.appendChild(authorTd)
+
+        const pagesTd = document.createElement('td');
+        pagesTd.innerHTML = book.getPages();
+        newTr.appendChild(pagesTd);
+
+        const readTd = document.createElement('td');
+        if (book.getRead() == true) {
+            readTd.innerHTML = "<input type=\"checkbox\" class=\"read\" checked></input>"
+        } 
+        else {
+            readTd.innerHTML = "<input type=\"checkbox\" class=\"read\"></input>"
+        }
+        newTr.appendChild(readTd);
+
+        //create functioning delete button
+        const delTd = document.createElement('td');
+        const delButton = document.createElement('input'); //image button to respond to 'click' to delete row and book
+        delButton.type = "image";
+        delButton.src="./images/trash-can-outline.png";
+        delButton.classList.add = "delButton";
+        delButton.alt = "Delete Book";
+        delButton.id = book.getID();
+        console.log(delButton.id);
+        delTd.appendChild(delButton);
+
+        newTr.appendChild(delTd);
+        console.log(newTr);
+        //add event listener for when delete button is clicked
+        delButton.addEventListener('click', () => deleteBook(book));
+
+        tbody.appendChild(newTr);//append new tr to tbody
+
     }
-    newTr.appendChild(readTd);
+    const deleteBook = (book) => {
+        let id = book.getID();
+        console.log(id);
+        const delRow = document.querySelector(`#tbody tr:nth-child(${id})`);
+        console.log(delRow);
 
-    const delTd = document.createElement('td');
+        delRow.remove(); //remove corres tr from table
+        libraryCreator.deleteBook(id); //remove at index
+        
+        //libraryCreator.getLib().forEach(book => console.log(book.getTitle()));
+    }   
 
-    const delButton = document.createElement('input'); //image button to respond to 'click' to delete row and book
-    delButton.type = "image";
-    delButton.src="./images/trash-can-outline.png";
-    delButton.classList.add = "delButton";
-    delButton.alt = "Delete Book";
-    delTd.appendChild(delButton);
+    return { loadTable, deleteBook };
+})();
 
-    myDelButtons.push(delButton); //append new button to array of delete buttons as well
-    newTr.appendChild(delTd);
+const loader = (() => {
+    const add = document.querySelector("#add");
+    const inputTitle = document.querySelector("#title");
+    const inputAuthor = document.querySelector("#author");
+    const inputPages = document.querySelector("#pages");
+    const inputRead = document.querySelector('#read');
 
-    myLibrary.push(book); //add book to myLibrary
-    //newTr.id=myLibrary.indexOf(book); //add id to each row corresponding to index in myLibrary
-    delButton.id = `${myLibrary.indexOf(book)}`; //correspond id w/ index of book in myLibrary
-    book.id = myLibrary.indexOf(book);
-    console.log(book.id);
+    const clearFields = () => {
+        inputTitle.value = '';
+        inputAuthor.value = '';
+        inputPages.value = '';
+        inputRead.checked = false;
+    }
 
-    delButton.addEventListener('click', book.deleteBook);
+    add.addEventListener("click", () => {  
+        let myBook = Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
+        libraryCreator.loadBook(myBook);
+        DOM_manager.loadTable(myBook);
+        clearFields();
+    });
+})();
 
-    tbody.appendChild(newTr);//make new tr and append to previous, include id of row# in tr
-
-
-}
-
-function clearFields() {
-    inputTitle.value = '';
-    inputAuthor.value = '';
-    inputPages.value = '';
-    inputRead.checked = false;
-}
-
-let myDelButtons = []; //create array of delete buttons that corresponds by index w/ books in myLibrary
-let myLibrary = [];
-let table = document.querySelector('#table');
-let tbody = document.querySelector('#tbody');
-
-const add = document.querySelector("#add");
-const inputTitle = document.querySelector("#title");
-const inputAuthor = document.querySelector("#author");
-const inputPages = document.querySelector("#pages");
-const inputRead = document.querySelector('#read');
-
-let book1 = new Book('1Q84', 'Murakami', 928, true);
-let book2 = new Book('The Alchemist', 'Coelho', 197, false);
-loadTable(book1);
-loadTable(book2);
-let isChecked = document.querySelectorAll('.read');
-
-add.addEventListener("click", () =>  {  
-    let myBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked)
-    loadTable(myBook);
-    clearFields();
-});
-console.log(myLibrary);
-
-// console.log(isChecked);
-// console.log(isChecked[0]);
-// isChecked.addEventListener('change', this.changeRead);
+let book1 = Book('1Q84', 'Murakami', 928, true);
+let book2 = Book('The Alchemist', 'Coelho', 197, false);
+libraryCreator.loadBook(book1);
+libraryCreator.loadBook(book2);
+DOM_manager.loadTable(book1);
+DOM_manager.loadTable(book2);
